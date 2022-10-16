afc9c5b95c5cadfdff3afc0c0d21e69a4b9f236f
Crepper710 <65467857+Crepper710@users.noreply.github.com>
2022-10-16 17:56:29 +0200

    changed code to work for 1.16.5

--- src/main/java/io/github/crepper710/no_f3/mixin/MixinPlayer.java
+++ src/main/java/io/github/crepper710/no_f3/mixin/MixinPlayer.java
@@ -1,13 +1,13 @@
 package io.github.crepper710.no_f3.mixin;
 
 import io.github.crepper710.no_f3.Config;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Player.class)
+@Mixin(PlayerEntity.class)
 public class MixinPlayer {
     @Inject(method = "isReducedDebugInfo", at = @At("RETURN"), cancellable = true)
     public void isReducedDebugInfo(CallbackInfoReturnable<Boolean> cir) {
